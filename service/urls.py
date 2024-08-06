d924bc0d0d13db57336a56d8c35fb50e9e333ae9
Sergiy Yurov <urovsergej10@gmail.com>
2024-08-06 16:59:40 +0300

    add dish-list url and dish-detail url to service/urls.py

--- service/urls.py
+++ service/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from service.views import index, DishTypesListView, DishListView
+from service.views import index, DishTypesListView, DishListView, DishDetailView
 
 urlpatterns = [
     path("", index, name="index"),
     path("dish_types/", DishTypesListView.as_view(), name="dish-types-list"),
     path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("dish/<int:pk>", DishDetailView.as_view(), name="dish-detail"),
 ]
 
 app_name = "service"
