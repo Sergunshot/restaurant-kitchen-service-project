21efd87c75281509b0786a8818f3128d16691741
Sergiy Yurov <urovsergej10@gmail.com>
2024-08-06 18:35:58 +0300

    add 2 urls cook-list, cook-detail to service/urls.py

--- service/urls.py
+++ service/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
-from service.views import index, DishTypesListView, DishListView, DishDetailView
+from service.views import index, DishTypesListView, DishListView, DishDetailView, CooksListView, \
+    CookDetailView
 
 urlpatterns = [
     path("", index, name="index"),
     path("dish_types/", DishTypesListView.as_view(), name="dish-types-list"),
     path("dishes/", DishListView.as_view(), name="dish-list"),
     path("dish/<int:pk>", DishDetailView.as_view(), name="dish-detail"),
+    path("cooks/", CooksListView.as_view(), name="cooks-list"),
+    path("cooks/<int:pk>", CookDetailView.as_view(), name="cooks-detail"),
 ]
 
 app_name = "service"
