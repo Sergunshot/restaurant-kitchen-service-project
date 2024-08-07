57e346eb3cd2a7ae4820841aee03a83b3b65550a
Sergiy Yurov <urovsergej10@gmail.com>
2024-08-07 14:04:04 +0300

    add home page visits counter modify index view and index.html

--- service/views.py
+++ service/views.py
@@ -10,10 +10,13 @@ def index(request: HttpRequest) -> HttpResponse:
     num_cooks = Cook.objects.count()
     num_dish_types = DishType.objects.count()
     num_dishes = Dish.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     context = {
         "num_cooks": num_cooks,
         "num_dish_types": num_dish_types,
-        "num_dishes": num_dishes
+        "num_dishes": num_dishes,
+        "num_visits": num_visits,
     }
     return render(request, "service/index.html", context)
 
