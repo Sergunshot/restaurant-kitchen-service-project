013bf0768bbd3634b54516ed21d27582706f0cf5
Sergiy Yurov <urovsergej10@gmail.com>
2024-08-08 19:20:36 +0300

    add dish-create ur to service/urls.py

--- service/urls.py
+++ service/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 
 from service.views import index, DishTypesListView, DishListView, DishDetailView, CooksListView, \
-    CookDetailView, DishTypeCreateView, DishTypeUpdateView, DishTypeDeleteView
+    CookDetailView, DishTypeCreateView, DishTypeUpdateView, DishTypeDeleteView, DishCreateView
 
 urlpatterns = [
     path("", index, name="index"),
     path("dish_types/", DishTypesListView.as_view(), name="dish-types-list"),
-    path("dishes/", DishListView.as_view(), name="dish-list"),
     path("dish_type/create/", DishTypeCreateView.as_view(), name="dish-type-create"),
     path("dish_type/<int:pk>/update/", DishTypeUpdateView.as_view(), name="dish-type-update"),
     path("dish_type/<int:pk>/delete/", DishTypeDeleteView.as_view(), name="dish-type-delete"),
+    path("dishes/", DishListView.as_view(), name="dish-list"),
     path("dish/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+    path("dish/create/", DishCreateView.as_view(), name="dish-create"),
     path("cooks/", CooksListView.as_view(), name="cooks-list"),
     path("cooks/<int:pk>/", CookDetailView.as_view(), name="cooks-detail"),
 ]
