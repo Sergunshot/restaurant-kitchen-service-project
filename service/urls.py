4af21913d0f9aba05ae34b71a31eeff007ad8fc4
Sergiy Yurov <urovsergej10@gmail.com>
2024-08-08 19:06:48 +0300

    add 2 urls dish-type-update, dish-type-delete

--- service/urls.py
+++ service/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
 
 from service.views import index, DishTypesListView, DishListView, DishDetailView, CooksListView, \
-    CookDetailView, DishTypeCreateView
+    CookDetailView, DishTypeCreateView, DishTypeUpdateView, DishTypeDeleteView
 
 urlpatterns = [
     path("", index, name="index"),
     path("dish_types/", DishTypesListView.as_view(), name="dish-types-list"),
     path("dishes/", DishListView.as_view(), name="dish-list"),
     path("dish_type/create/", DishTypeCreateView.as_view(), name="dish-type-create"),
-    path("dish/<int:pk>", DishDetailView.as_view(), name="dish-detail"),
+    path("dish_type/<int:pk>/update/", DishTypeUpdateView.as_view(), name="dish-type-update"),
+    path("dish_type/<int:pk>/delete/", DishTypeDeleteView.as_view(), name="dish-type-delete"),
+    path("dish/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
     path("cooks/", CooksListView.as_view(), name="cooks-list"),
-    path("cooks/<int:pk>", CookDetailView.as_view(), name="cooks-detail"),
+    path("cooks/<int:pk>/", CookDetailView.as_view(), name="cooks-detail"),
 ]
 
 app_name = "service"
