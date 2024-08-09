75d669a0308d259357e38da828bfb9b5404be38d
Sergiy Yurov <urovsergej10@gmail.com>
2024-08-09 17:14:17 +0300

    modify CookListView to get search form

--- service/views.py
+++ service/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from service.forms import CookForm, DishForm, DishSearchForm, IngredientSearchForm
+from service.forms import CookForm, DishForm, DishSearchForm, IngredientSearchForm, CookSearchForm
 from service.models import Cook, DishType, Dish, Ingredient
 
 
@@ -107,6 +107,21 @@ class CookListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 7
     template_name = "service/cook_list.html"
     context_object_name = "cook_list"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CookListView, self).get_context_data(**kwargs)
+        username = self.request.GET.get("username", "")
+        context["search_form"] = CookSearchForm(
+            initial={"username": username}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Cook.objects.all()
+        form = CookSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(username__icontains=form.cleaned_data["username"])
+        return queryset
 
 
 class CookDetailView(LoginRequiredMixin, generic.DetailView):
