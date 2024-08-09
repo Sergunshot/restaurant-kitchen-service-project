231721cd88aa4a44e02a4c9a190d3ae7afdd9b8e
Sergiy Yurov <urovsergej10@gmail.com>
2024-08-09 16:49:14 +0300

    add DishSearchForm to service/forms.py

--- service/forms.py
+++ service/forms.py
@@ -1,9 +1,37 @@
 from django.contrib.auth.forms import UserCreationForm
-
-from service.models import Cook
+from django import forms
+from service.models import Cook, Dish, Ingredient
 
 
 class CookForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Cook
-        fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "years_of_experience", )
+        fields = UserCreationForm.Meta.fields + ("first_name",
+                                                 "last_name",
+                                                 "years_of_experience", )
+
+
+class DishForm(forms.ModelForm):
+    ingredients = forms.ModelMultipleChoiceField(
+        queryset=Ingredient.objects.all(),
+        widget=forms.CheckboxSelectMultiple(),
+        required=False
+    )
+    cooks = forms.ModelMultipleChoiceField(
+        queryset=Cook.objects.all(),
+        widget=forms.CheckboxSelectMultiple(),
+        required=False
+    )
+
+    class Meta:
+        model = Dish
+        fields = "__all__"
+
+
+class DishSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=100,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"})
+    )
