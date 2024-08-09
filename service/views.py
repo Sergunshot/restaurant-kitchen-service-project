4a2f79f023f94f990aee914a74d65cae5503dc6d
Sergiy Yurov <urovsergej10@gmail.com>
2024-08-09 17:00:56 +0300

    modify IngredientViewList to get search form

--- service/views.py
+++ service/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from service.forms import CookForm, DishForm, DishSearchForm
+from service.forms import CookForm, DishForm, DishSearchForm, IngredientSearchForm
 from service.models import Cook, DishType, Dish, Ingredient
 
 
@@ -124,6 +124,21 @@ class IngredientListView(LoginRequiredMixin, generic.ListView):
     model = Ingredient
     paginate_by = 10
     template_name = "service/ingredient_list.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(IngredientListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = IngredientSearchForm(
+            initial={"name": name}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Ingredient.objects.all()
+        form = IngredientSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(name__icontains=form.cleaned_data["name"])
+        return queryset
 
 
 class IngredientCreateView(LoginRequiredMixin, generic.CreateView):
