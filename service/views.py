bdc5a5a5405671fd19fc59290a3712e4fa0dc89c
Sergiy Yurov <urovsergej10@gmail.com>
2024-08-09 05:14:04 +0300

    add IngredientListView to service/views.py

--- service/views.py
+++ service/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from service.models import Cook, DishType, Dish
+from service.models import Cook, DishType, Dish, Ingredient
 
 
 @login_required
@@ -14,6 +14,7 @@ def index(request: HttpRequest) -> HttpResponse:
     num_cooks = Cook.objects.count()
     num_dish_types = DishType.objects.count()
     num_dishes = Dish.objects.count()
+    num_ingredients = Ingredient.objects.count()
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
     context = {
@@ -21,41 +22,42 @@ def index(request: HttpRequest) -> HttpResponse:
         "num_dish_types": num_dish_types,
         "num_dishes": num_dishes,
         "num_visits": num_visits,
+        "num_ingredients": num_ingredients,
     }
     return render(request, "service/index.html", context)
 
 
-class DishTypesListView(LoginRequiredMixin, generic.ListView):
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
     model = DishType
-    paginate_by = 5
-    template_name = "service/dish_types_list.html"
+    paginate_by = 7
+    template_name = "service/dish_type_list.html"
     queryset = DishType.objects.all()
-    context_object_name = "dish_types_list"
+    context_object_name = "dish_type_list"
 
 
 class DishTypeCreateView(LoginRequiredMixin, generic.edit.CreateView):
     model = DishType
-    success_url = reverse_lazy("service:dish-types-list")
+    success_url = reverse_lazy("service:dish-type-list")
     fields = "__all__"
     template_name = "service/dish_type_form.html"
 
 
 class DishTypeUpdateView(LoginRequiredMixin, generic.edit.UpdateView):
     model = DishType
-    success_url = reverse_lazy("service:dish-types-list")
+    success_url = reverse_lazy("service:dish-type-list")
     fields = "__all__"
     template_name = "service/dish_type_form.html"
 
 
 class DishTypeDeleteView(LoginRequiredMixin, generic.edit.DeleteView):
     model = DishType
-    success_url = reverse_lazy("service:dish-types-list")
+    success_url = reverse_lazy("service:dish-type-list")
     template_name = "service/dish_type_confirm_delete.html"
 
 
 class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
-    paginate_by = 5
+    paginate_by = 7
     template_name = "service/dish_list.html"
     queryset = Dish.objects.select_related('dish_type').prefetch_related("ingredients")
     context_object_name = "dish_list"
@@ -85,13 +87,19 @@ class DishDeleteView(LoginRequiredMixin, generic.edit.DeleteView):
     template_name = "service/dish_confirm_delete.html"
 
 
-class CooksListView(LoginRequiredMixin, generic.ListView):
+class CookListView(LoginRequiredMixin, generic.ListView):
     model = Cook
-    paginate_by = 5
-    template_name = "service/cooks_list.html"
+    paginate_by = 7
+    template_name = "service/cook_list.html"
     context_object_name = "cook_list"
 
 
 class CookDetailView(LoginRequiredMixin, generic.DetailView):
     model = Cook
     queryset = Cook.objects.prefetch_related("dishes")
+
+
+class IngredientListView(LoginRequiredMixin, generic.ListView):
+    model = Ingredient
+    paginate_by = 10
+    template_name = "service/ingredient_list.html"
