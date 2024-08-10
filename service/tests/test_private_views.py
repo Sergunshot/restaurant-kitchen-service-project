0e743a1e258c40bcec28776bd0deec26ca559bb7
Sergiy Yurov <urovsergej10@gmail.com>
2024-08-11 00:17:50 +0300

    add tests for CookViews

--- service/tests/test_private_views.py
+++ service/tests/test_private_views.py
@@ -77,12 +77,19 @@ class ViewsPrivateTest(TestCase):
         response = self.client.get(DISH_VIEW_LIST_URL)
         self.assertEqual(response.status_code, 200)
         list_dishes = Dish.objects.all()
-        self.assertEqual(list(response.context["dish_list"]), list(list_dishes))
+        self.assertEqual(
+            list(response.context["dish_list"]),
+            list(list_dishes)
+        )
         self.assertTemplateUsed(response, "service/dish_list.html")
 
     def test_create_dish_view(self):
         dish_type = DishType.objects.create(name="desserts")
-        dish = Dish.objects.create(name="ice cream", dish_type=dish_type, price=10)
+        dish = Dish.objects.create(
+            name="ice cream",
+            dish_type=dish_type,
+            price=10
+        )
         response = self.client.get(DISH_CREATE_VIEW_URL)
         self.assertEqual(response.status_code, 200)
         res = self.client.get(DISH_VIEW_LIST_URL)
@@ -94,7 +101,11 @@ class ViewsPrivateTest(TestCase):
     def test_update_dish_view(self):
         dish_type = DishType.objects.create(name="desserts")
         dish_type_second = DishType.objects.create(name="pizza")
-        dish = Dish.objects.create(name="Carbonara", dish_type=dish_type, price=10)
+        dish = Dish.objects.create(
+            name="Carbonara",
+            dish_type=dish_type,
+            price=10
+        )
         dish_update_url_view = reverse(
             "service:dish-update",
             kwargs={"pk": dish.id}
@@ -112,7 +123,11 @@ class ViewsPrivateTest(TestCase):
 
     def test_dish_delete_view(self):
         dish_type = DishType.objects.create(name="pizza")
-        dish = Dish.objects.create(name="Carbonara", dish_type=dish_type, price=10)
+        dish = Dish.objects.create(
+            name="Carbonara",
+            dish_type=dish_type,
+            price=10
+        )
         dish_delete_url_view = reverse(
             "service:dish-delete",
             kwargs={"pk": dish.id}
@@ -152,4 +167,90 @@ class ViewsPrivateTest(TestCase):
         self.assertContains(response, cook.last_name)
         self.assertTemplateUsed(response, "service/dish_detail.html")
 
+    def test_retrieve_cook_list_view(self):
+        Cook.objects.create(
+            username="Lamuk",
+            first_name="Dima",
+            last_name="Dobkin",
+            years_of_experience=3
+        )
+        Cook.objects.create(username="Sergun",
+                            first_name="Sergiy",
+                            last_name="Yurov",
+                            years_of_experience=5
+                            )
+        response = self.client.get(COOK_VIEW_LIST_URL)
+        self.assertEqual(response.status_code, 200)
+        list_cooks = Cook.objects.all()
+        self.assertEqual(list(response.context["cook_list"]),
+                         list(list_cooks))
+        self.assertTemplateUsed(response, "service/cook_list.html")
 
+    def test_driver_create(self):
+        form_data = {
+            "username": "valuc",
+            "password1": "Test22021994",
+            "password2": "Test22021994",
+            "first_name": "Dmitriy",
+            "last_name": "Dobkin",
+            "years_of_experience": 3,
+        }
+        self.client.post(COOK_CREATE_VIEW_URL, data=form_data)
+        new_user = get_user_model().objects.get(username=form_data["username"])
+        response = self.client.get(COOK_CREATE_VIEW_URL)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "service/cook_form.html")
+        self.assertEqual(new_user.username, form_data["username"])
+        self.assertEqual(new_user.first_name, form_data["first_name"])
+        self.assertEqual(new_user.last_name, form_data["last_name"])
+        self.assertEqual(
+            new_user.years_of_experience,
+            form_data["years_of_experience"]
+        )
+
+    def test_driver_delete(self):
+        cook = Cook.objects.create(
+            username="Kolin",
+            first_name="Dima",
+            last_name="Durbin",
+            years_of_experience=3
+        )
+        driver_delete_url = reverse("service:cook-delete",
+                                    kwargs={"pk": cook.id})
+        response = self.client.get(driver_delete_url)
+        self.assertEqual(response.status_code, 200)
+        Cook.objects.filter(pk=cook.id).delete()
+        res = self.client.get(COOK_VIEW_LIST_URL)
+        self.assertNotContains(res, cook)
+        self.assertTemplateUsed(response, "service/cook_confirm_delete.html")
+
+    def test_cook_detail_view(self):
+        cook = Cook.objects.create(
+            username="Kolin",
+            first_name="Dima",
+            last_name="Durbin",
+            years_of_experience=3
+        )
+        dish_type = DishType.objects.create(name="pizza")
+        dish = Dish.objects.create(
+            name="Carbonara",
+            dish_type=dish_type,
+            price=10
+        )
+        dish.cooks.add(cook)
+        cook_detail_url_view = reverse("service:cook-detail",
+                                       kwargs={"pk": cook.id})
+        response = self.client.get(cook_detail_url_view)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, dish.name)
+        self.assertContains(response, dish.dish_type)
+        self.assertTemplateUsed(response, "service/cook_detail.html")
+
+    def test_cook_years_of_experience_update_view(self):
+        cook = Cook.objects.create(
+            username="Kolin",
+            years_of_experience=5
+        )
+        cook.years_of_experience = 10
+        cook.save()
+        self.assertTrue(cook.years_of_experience, 10)
