1d43d6c39d015076dd1cc71fe5bcde0d67ef3fe7
Sergiy Yurov <urovsergej10@gmail.com>
2024-09-06 17:46:13 +0300

    style: Modify admin.py by code style

--- service/admin.py
+++ service/admin.py
@@ -21,7 +21,8 @@ class DishAdmin(admin.ModelAdmin):
 class CookAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("years_of_experience", )
     fieldsets = UserAdmin.fieldsets + (("Additional Info",
-                                        {"fields": ("years_of_experience", )}),)
+                                        {"fields": (
+                                            "years_of_experience", )}),)
     add_fieldsets = UserAdmin.add_fieldsets + (("Additional Info", {"fields": (
         "first_name",
         "last_name",
