762ac3c8a2bf73f216064be4b1d46be741c5f6cb
Sergiy Yurov <urovsergej10@gmail.com>
2024-11-27 23:15:55 +0200

    Add seetingsapp

---
+++ restaurant_kitchen_service/settings/dev.py
@@ -0,0 +1,16 @@
+from .base import *
+from decouple import config
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config("DEBUG", cast=bool)
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
